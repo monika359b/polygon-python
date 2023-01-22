45aa2cd3ac641a890ede4aac1293ee7a0d31ef6c
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-22 17:04:08 +0530

    Create app.py

--- app.py
+++ app.py
@@ -1,40 +1,41 @@
+import json
+from concurrent.futures import ThreadPoolExecutor
+import time
+from telethon import TelegramClient, events
 import telethon
 import asyncio
-import json
-import requests
-from telethon import TelegramClient, events
-
-async def start_event_handler(client,bot_token,chat_id):
-    try:
-        await client.start(bot_token=bot_token)
-        @client.on(events.NewMessage(chats=chat_id))
-        async def my_event_handler(event):
-            if event.message.from_id.user_id == chat_id:
-                if '#AD' in event.message.message or '#paidAD' in event.message.message or 'sponsored' in event.message.message:
-                    await client.delete_messages(event.message.peer_id, event.message.id)
-                    print('message deleted successfully')
-                else:
-                    print('failed')
-                    pass
-        client.add_event_handler(my_event_handler)
-    except telethon.errors.rpcerrorlist.AccessTokenInvalidError:
-        print("Invalid token, skipping")
-        url = "https://bb-ad-blocker.vercel.app/remove="+bot_token
-        requests.get(url)
-        return
 
 async def main():
     with open('tokens.json', 'r') as f:
         tokens = json.load(f)
-    tasks = []
     for token in tokens:
         bot_token = token["token"]
         chat_id = int(bot_token.split(':')[0])
-        client = TelegramClient("Sot"+bot_token, "7243983", "e1bd137a37da9f95ecd2b206a8c8be48")
-        tasks.append(start_event_handler(client,bot_token,chat_id))
-    await asyncio.gather(*tasks)
+        asyncio.create_task(run_client(bot_token, chat_id))
     while True:
-        await asyncio.sleep(5) # sleep for 30 seconds before checking for new
+        await asyncio.sleep(10)
+
+async def run_client(bot_token, chat_id):
+    while True:
+        try:
+            client = TelegramClient('Sot', "7243983", "e1bd137a37da9f95ecd2b206a8c8be48")
+            await client.start(bot_token=bot_token)
+
+            @client.on(events.NewMessage(chats=chat_id))
+            async def my_event_handler(event):
+                if event.message.from_id.user_id == chat_id:
+                    if '#AD' in event.message.message or '#paidAD' in event.message.message or 'sponsored' in event.message.message:
+                        print(event)
+                        await client.delete_messages(event.message.peer_id.user_id, event.message.id)
+                        print('message deleted successfully')
+                    else:
+                        print('failed')
+                        pass
+            client.add_event_handler(my_event_handler)
+            await client.run_until_disconnected()
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(10)
 
 if __name__ == '__main__':
     asyncio.run(main())
