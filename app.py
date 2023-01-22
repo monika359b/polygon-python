fd1a086ce5a03760bdf4f52197d881083168f5f8
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-22 13:27:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+import telethon
 import asyncio
 import json
 from telethon import TelegramClient, events
 
 async def start_event_handler(client,bot_token,chat_id):
-    await client.start(bot_token=bot_token)
-    @client.on(events.NewMessage(chats=chat_id))
-    async def my_event_handler(event):
-        if event.message.from_id.user_id == chat_id:
-            if '#AD' in event.message.message or '#paidAD' in event.message.message or 'sponsored' in event.message.message:
-                await client.delete_messages(event.message.peer_id, event.message.id)
-                print('message deleted successfully')
-            else:
-                print('failed')
-                pass
-    client.add_event_handler(my_event_handler)
+    try:
+        await client.start(bot_token=bot_token)
+        @client.on(events.NewMessage(chats=chat_id))
+        async def my_event_handler(event):
+            if event.message.from_id.user_id == chat_id:
+                if '#AD' in event.message.message or '#paidAD' in event.message.message or 'sponsored' in event.message.message:
+                    await client.delete_messages(event.message.peer_id, event.message.id)
+                    print('message deleted successfully')
+                else:
+                    print('failed')
+                    pass
+        client.add_event_handler(my_event_handler)
+    except telethon.errors.rpcerrorlist.AccessTokenInvalidError:
+        print("Invalid token, skipping")
+        return
 
 async def main():
     with open('tokens.json', 'r') as f:
