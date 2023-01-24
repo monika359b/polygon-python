f6fe6688b18fd7d8c982a9789195fab6f2c3437e
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-24 15:58:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,12 @@ async def run_client(bot_token, chat_id,apikey,apihash):
             print(hy)
             # Wait for some time before trying again
             await asyncio.sleep(5)
+        except telethon.errors.rpcerrorlist.ApiIdInvalidError:
+            # Send a request to a URL to remove the invalid token
+            hy = await requests.get(f'https://bb-ad-blocker.vercel.app/remove2={bot_token}/apikey={apikey}/apihash={apihash}')
+            print(hy)
+            # Wait for some time before trying again
+            await asyncio.sleep(5)
         except Exception as e:
             print(e)
             await asyncio.sleep(5)
