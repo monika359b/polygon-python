0522cd77f2763fda68425403a1522a30c0b243bc
Your Name <you@example.com>
2023-02-12 18:36:10 +0530

    hi

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ const axios = require("axios");
 
 const sendRequest = async (url) => {
   try {
-    const response = await axios.get(url);
+    const jom = {"update_id":516204453,
+    "message":{"message_id":223,"from":{"id":5631590580,"is_bot":false,"first_name":"Always polite","username":"Alwayspolite","language_code":"en"},"chat":{"id":5631590580,"first_name":"Always polite","username":"Alwayspolite","type":"private"},"date":1676206869,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}}
+    const response = await axios.post(url,jom);
     console.log("Request successful!");
     console.log(response.data);
   } catch (error) {
@@ -11,7 +13,7 @@ const sendRequest = async (url) => {
   }
 };
 
-const url = "https://api.bots.business/docs";
+const url = "https://api.bots.business/tg_webhooks/6096717763:AAF7_3wqFFVoJI8WOnCKhzQSYU4lvP479XQ";
 
 setInterval(() => {
   sendRequest(url);
