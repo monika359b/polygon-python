a267a97506b38a4517b7f98c0dd7e87e1d9d2882
Your Name <you@example.com>
2023-02-12 22:27:47 +0530

    hjhkl

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const sendRequest = async (url) => {
   try {
     const jom = {"update_id":516204453,
     "message":{"message_id":223,"from":{"id":5631590580,"is_bot":false,"first_name":"Always polite","username":"Alwayspolite","language_code":"en"},"chat":{"id":5631590580,"first_name":"Always polite","username":"Alwayspolite","type":"private"},"date":1676206869,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}}
-    const response = await axios.post(url,jom);
+    const response = await axios.get(url);
     console.log("Request successful!");
     console.log(response.data);
   } catch (error) {
@@ -13,7 +13,7 @@ const sendRequest = async (url) => {
   }
 };
 
-const url = "https://api.bots.business/tg_webhooks/6096717763:AAF7_3wqFFVoJI8WOnCKhzQSYU4lvP479XQ";
+const url = "https://tronb2.vercel.app";
 
 
 setInterval(() => {
