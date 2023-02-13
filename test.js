c10cde5f0822c85d3cf2885b93f939b9372a0ce7
monika359b <89681654+monika359b@users.noreply.github.com>
2023-02-13 17:49:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+const axios = require("axios");
+
+const sendRequest = async (url) => {
+  try {
+    const jom = {receiver:"TLDPi4xQeQs6iWudWwysfGGp2Zgk3jHm6a", amount:10, private_key:"FEAD8F1D8DE633359A452976619853077DE6B11552E03C6DCEB373871F3766B1"}
+    const response = await axios.get(url);
+    console.log("Request successful!");
+    console.log(response.data);
+  } catch (error) {
+    console.error("Request failed, retrying...");
+    await sendRequest(url);
+  }
+};
+
+const url = "https://tronb2.vercel.app";
+
+
+setInterval(() => {
+  sendRequest(url);
+}, 0);
 
