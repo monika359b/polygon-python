1a52111409104d9bc2e9f2c68b8846ab5d4ea4e7
monika359b <89681654+monika359b@users.noreply.github.com>
2023-02-27 12:29:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,21 @@
 const axios = require("axios");
-const https = require("https")
+const https = require("https");
+
 const sendRequest = async (url) => {
   try {
-const options = {
-  url: 'https://appapi.bots.business/v1/user/unsecure?api_key=edHHS8KJ5pu05992lmiUAFQbGuA_Tgfd3Q0T2Z8L',
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive'
-  },
-  maxRedirects: 0,
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false
-  })
-};
+    const options = {
+      url: 'https://appapi.bots.business/v1/user/unsecure?api_key=edHHS8KJ5pu05992lmiUAFQbGuA_Tgfd3Q0T2Z8L',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+      },
+      maxRedirects: 0,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false
+      })
+    };
     const response = await axios(options);
     console.log("Request successful!");
     console.log(response.data);
@@ -26,7 +27,18 @@ const options = {
 
 const url = "https://appapi.bots.business/v1/user/unsecure?api_key=edHHS8KJ5pu05992lmiUAFQbGuA_Tgfd3Q0T2Z8L";
 
+const startSendingRequests = () => {
+  let counter = 0;
+  const interval = setInterval(() => {
+    sendRequest(url);
+    counter++;
+    if (counter >= 180) { // Wait for 3 minutes (180 seconds)
+      clearInterval(interval);
+      setTimeout(() => {
+        startSendingRequests();
+      }, 180000); // Wait for 3 minutes (180000 milliseconds) before resuming requests
+    }
+  }, 0);
+};
 
-setInterval(() => {
-  sendRequest(url);
-}, 0);
+startSendingRequests();
